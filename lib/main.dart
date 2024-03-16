e10cfcbdd47a9f9b7a18b7aee72721660495d8cd
Aswgt999 <108606894+Aswgt999@users.noreply.github.com>
2024-03-16 15:26:57 +0530

    updated reg_page

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:qr_app1/login_page.dart';
-import 'package:qr_app1/registration_page.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: RegistrationPage(),
+    home: LoginPage(),
   ));
 }
